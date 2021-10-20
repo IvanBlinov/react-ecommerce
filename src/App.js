6288bfb5ab33dc51c2b0b770ad42ee71b1891b96
Ivan <ivan@Ivans-MacBook-Pro.local>
2021-10-19 22:15:09 -0700

    Added email and password signup and signin

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import SignInPage from "./pages/signin/signin.component";
-import { auth } from "./firebase/firebase.utils";
+import SignInPage from "./pages/signin/sign-in-and-sign-up.component";
+import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
 
 class App extends React.Component {
 
@@ -21,8 +21,21 @@ class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState( { currentUser: user });
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+
+                userRef.onSnapshot(snapshot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapshot.id,
+                            ...snapshot.data
+                        }
+                    })
+                })
+            }
+
+            this.setState({currentUser: userAuth});
         });
     }
 
--- src/pages/signin/sign-in-and-sign-up.component.jsx
+++ src/pages/signin/sign-in-and-sign-up.component.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-import './signin.styles.scss';
+import './sign-in-and-sign-up.styles.scss';
 
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const SignInPage = () => (
     <div  className='sign-in-and-sign-up'>
         <SignIn />
+        <SignUp />
     </div>
 );
 
