4d1cc1531100c706349bd6ecdf3483ba381831d7
Ivan <ivan@Ivans-MacBook-Pro.local>
2021-10-21 23:46:35 -0700

    Refactoring

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import CheckoutPage from "./pages/checkout/checkout.component";
-import SignInAndSignUpPage from "./pages/signin/sign-in-and-sign-up.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 class App extends React.Component {
 
@@ -49,7 +49,7 @@ class App extends React.Component {
                 <Header />
                 <Switch>
                     <Route exact path='/' component={HomePage} />
-                    <Route exact path='/shop' component={ShopPage} />
+                    <Route path='/shop' component={ShopPage} />
                     <Route exact path='/checkout' component={CheckoutPage} />
                     <Route exact path='/signin' render={() => this.props.currentUser ? <Redirect to='/' /> : <SignInAndSignUpPage />} />
                 </Switch>
